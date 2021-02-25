74602c1374d3e56ce402a6a75cac64aff43899e7
JohnnHidalgo <johnna.7h@gmail.com>
2021-02-25 11:27:48 -0400

    add ejercicio 7

---
+++ ModeladoJava/src/hallarparametros/Ejercicio7.java
@@ -0,0 +1,38 @@
+package hallarparametros;
+
+import java.io.File;
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Ejercicio7 {
+
+	static ArrayList<Double> promedioNotas = new ArrayList<>();
+	static ArrayList<Double> edad = new ArrayList<>();
+	static ArrayList<Double> numeroMaterias = new ArrayList<>();
+	static ArrayList<Double> horasEstudio = new ArrayList<>();
+	static ArrayList<Double> recreo = new ArrayList<>();
+	static ArrayList<Double> numeroAmigos = new ArrayList<>();
+	
+	public static void main(String[] args) throws Exception {
+		readDataFromCsv();
+		Operaciones.informeEjercicio7(promedioNotas, edad, numeroMaterias);		
+
+	}
+	
+	public static void readDataFromCsv() throws Exception {
+		String[] row = new String[6];
+		Scanner sc = new Scanner(new File("regresionlineal/datos.csv"));  
+		while (sc.hasNext()){
+			row = sc.next().split(",", 6);
+			promedioNotas.add(Double.parseDouble(row[0]));
+			edad.add(Double.parseDouble(row[1]));
+			numeroMaterias.add(Double.parseDouble(row[2]));
+			horasEstudio.add(Double.parseDouble(row[3]));
+			recreo.add(Double.parseDouble(row[4]));
+			numeroAmigos.add(Double.parseDouble(row[5]));
+		}
+		sc.close();
+	}	
+
+	
+}
--- ModeladoJava/src/hallarparametros/Operaciones.java
+++ ModeladoJava/src/hallarparametros/Operaciones.java
@@ -4,32 +4,24 @@ import java.util.ArrayList;
 
 public class Operaciones {
 	
-	public static String getEcuacion(ArrayList<Double> y, ArrayList<Double> x) {
-		String ecuacion;
-		ecuacion = "y = "+getB0(y, x)+" + "+getB1(y, x)+"x";
-		return ecuacion;
-	}
-
-
 	public static Double getParametroY(ArrayList<Double> y, ArrayList<Double> x) {
 		Double YW;
 		YW = getSumatoriaDeDosVariables(y,x) / getSumatoriaDatoAlCuadrado(x);
 		return YW;
 	}
 	
-	public static Double getB0(ArrayList<Double> y, ArrayList<Double> x) {
-		Double B0;
-		B0 = getPromedio(y) - (getB1(y, x) * getPromedio(x));
-		return B0;
-	}
-	
-	
-	public static Double getB1(ArrayList<Double> y, ArrayList<Double> x) {
+	public static Double getB1(ArrayList<Double> y, ArrayList<Double> x1, ArrayList<Double> x2) {
 		Double B1;
-		B1 = (getSumatoriaDeDosVariables(y, x)-(getPromedio(y)*getSumatoriaUnaVariable(x)))/(getSumatoriaDatoAlCuadrado(x)-(getPromedio(x)*getSumatoriaUnaVariable(x)));
+		B1 = (getSumatoriaDeDosVariables(y, x1)-getB2(y,x1,x2)*getSumatoriaDeDosVariables(x1, x2))/(getSumatoriaDatoAlCuadrado(x1));
 		return B1;
 	}
 	
+	
+	public static Double getB2(ArrayList<Double> y, ArrayList<Double> x1, ArrayList<Double> x2) {
+		Double B2;
+		B2 = (getSumatoriaDeDosVariables(y, x2)-getSumatoriaDatoAlCuadrado(x2)+getSumatoriaDeDosVariables(y, x1)*getSumatoriaDeDosVariables(x1, x2))/(getSumatoriaDatoAlCuadrado(x1)*getSumatoriaDatoAlCuadrado(x2)+getSumatoriaDeDosVariablesAlCuadrado(x2, x1));
+		return B2;
+	}
 	
 	public static Double getPromedio(ArrayList<Double> datos) {	
 		Double suma= 0.0;
@@ -64,6 +56,14 @@ public class Operaciones {
 		return suma;
 	}
 	
+	public static Double getSumatoriaDeDosVariablesAlCuadrado(ArrayList<Double> datosy, ArrayList<Double> datosx) {	
+		Double suma= 0.0;
+		for (int i=0; i<datosy.size();i++) {
+			suma=suma+(datosy.get(i)*datosx.get(i))*(datosy.get(i)*datosx.get(i));
+		}
+		return suma;
+	}
+	
 	
 	
 	public static void getSistemaEcuaciones(ArrayList<Double> datosy, ArrayList<Double> datosx1, ArrayList<Double> datosx2, ArrayList<Double> datosx3) {	
@@ -91,15 +91,31 @@ public class Operaciones {
 		
 	}
 	
-	public static void informeEjercicio6(ArrayList<Double> datosy, ArrayList<Double> datosx, String y, String x) {	
-		System.out.print("Valor del parámetro con respecto a :  "+y+" / "+x+":  ");
-		System.out.println(Operaciones.getParametroY(datosy, datosx));
-		System.out.println();		
-	}
+
 	
 	public static void informeEjercicio5(ArrayList<Double> datosy, ArrayList<Double> datosx1, ArrayList<Double> datosx2, ArrayList<Double> datosx3) {	
 		Operaciones.getSistemaEcuaciones(datosy, datosx1, datosx2, datosx3);
 		System.out.println();		
 	}
 
+	public static void informeEjercicio6(ArrayList<Double> datosy, ArrayList<Double> datosx, String y, String x) {	
+		System.out.print("Valor del parámetro con respecto a :  "+y+" / "+x+":  ");
+		System.out.println(Operaciones.getParametroY(datosy, datosx));
+		System.out.println();		
+	}
+	
+	public static void informeEjercicio7(ArrayList<Double> datosy, ArrayList<Double> datosx1, ArrayList<Double> datosx2) {
+		
+		System.out.println("B1 : " + getB1(datosy, datosx1, datosx2));
+		System.out.println("B2 : " + getB2(datosy, datosx1, datosx2));
+		System.out.println();	
+		System.out.println("y = " + getB1(datosy, datosx1, datosx2)+ " x1" + getB2(datosy, datosx1, datosx2)+" x2");
+		
+		System.out.println();		
+	}
 }
+
+/*
+	
+	
+*/
